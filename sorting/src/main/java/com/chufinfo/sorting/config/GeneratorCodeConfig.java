0781529e676ed30772c59870bebf72e4ad662c59
litongke <litongke@126.com>
2022-01-23 15:31:11 +0800

    Update GeneratorCodeConfig.java

--- sorting/src/main/java/com/chufinfo/sorting/config/GeneratorCodeConfig.java
+++ sorting/src/main/java/com/chufinfo/sorting/config/GeneratorCodeConfig.java
@@ -40,7 +40,7 @@ public class GeneratorCodeConfig {
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://36.133.243.45:3306/lims-sorting?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&useSSL=false&allowPublicKeyRetrieval=true");
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/lims-sorting?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&useSSL=false&allowPublicKeyRetrieval=true");
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("root");
